04effccd686b2b4c0020962ddaf4c31db321078a
lonelyprince7 <1056692290@qq.com>
2020-02-03 23:21:11 +0800

    add

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ python3 -u main.py \
 --bert_model \
 /root/bert-base-uncased.tar.gz \
 --do_eval --do_train --train_batch_size 4 \
---output_dir /root/trained_model \
+--output_dir /root/cloze_test/trained_model \
 --learning_rate 1e-5 --num_train_epochs 4
